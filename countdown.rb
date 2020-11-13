2e365bf2d9d51eafc520d7906d53e0c698a5fa2f
Ben Spotswood <spotswoodb@gmail.com>
2020-11-13 13:58:53 -0500

    commit tree 3e7e6aad0562ca6017c67fcd41543c8a3f95c847

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,17 @@
-# number = 10
-
-# def countdown_with_sleep(number)
-#     sleep 5.seconds
-# end
+def countdown_with_sleep(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        sleep(1)
+        number -= 1
+    end
+  "HAPPY NEW YEAR!"
+end
 
 def countdown(number)
-    While number > 0
+    while number > 0
         puts "#{number} SECOND(S)!"
         number -= 1
     end
-    return "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
+
